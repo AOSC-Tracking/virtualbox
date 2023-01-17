385c993da7efac5cf4cf5a702575923e6a1c9392
Christian Hesse <mail@eworm.de>
2023-01-17 22:04:08 +0100

    FROM ARCHLINUX: properly handle i3wm

--- src/VBox/Frontends/VirtualBox/src/widgets/UIMiniToolBar.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIMiniToolBar.cpp
@@ -1076,6 +1076,9 @@ void UIMiniToolBar::prepare()
     /* Enable translucency through Qt API if supported: */
     if (uiCommon().isCompositingManagerRunning())
         setAttribute(Qt::WA_TranslucentBackground);
+
+    if (NativeWindowSubsystem::X11WindowManagerType() == X11WMType_i3)
+        setWindowFlags(Qt::FramelessWindowHint);
 #endif /* VBOX_WS_NIX */
 
     /* Make sure we have no focus: */
